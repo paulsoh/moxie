89c3e0b83858bf9725f98d895bf7513a7dc70d10
yongdoree <yongdoree@gmail.com>
2016-04-06 16:48:44 +0900

    refactor: made thirdparty settings file for 3rd party app settings

--- moxie/moxie/settings/partials/__init__.py
+++ moxie/moxie/settings/partials/__init__.py
@@ -2,3 +2,4 @@ from .application import *
 from .auth import *
 from .database import *
 from .static import *
+from .thirdparty import *
---
+++ moxie/moxie/settings/partials/thirdparty.py
@@ -0,0 +1,2 @@
+# django-crispy-forms settings
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
