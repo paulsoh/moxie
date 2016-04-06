0623bb6757bd6eef43642b71e819e2ba305402f6
yongdoree <yongdoree@gmail.com>
2016-04-06 12:11:16 +0900

    modify: database now uses postgres, database defined in seperate .env file

--- moxie/moxie/settings/partials/database.py
+++ moxie/moxie/settings/partials/database.py
@@ -1,11 +1,12 @@
 import os
-from .application import BASE_DIR
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+    )
 }
