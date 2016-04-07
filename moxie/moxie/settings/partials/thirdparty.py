d5b667b907160d67720e4f3ac5b3b6083ce930b8
yongdoree <yongdoree@gmail.com>
2016-04-07 12:34:23 +0900

    Added facebook social login functionality

--- moxie/moxie/settings/partials/thirdparty.py
+++ moxie/moxie/settings/partials/thirdparty.py
@@ -1,7 +1,22 @@
+import os
+
 # django-crispy-forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # django-summernote settings
 SUMMERNOTE_CONFIG = {
     'iframe': False,
+}
+
+# SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+# SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+# Facebook
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('FACEBOOK_KEY')
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('FACEBOOK_SECRET')
+
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+FACEBOOK_EXTENDED_PERMISSIONS = ['picture']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id, name, email, age_range'
 }
