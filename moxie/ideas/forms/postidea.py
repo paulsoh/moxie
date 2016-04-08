39c8c74fb9abe2dadd5b1a483d74cb5309c33f8c
yongdoree <yongdoree@gmail.com>
2016-04-08 13:19:41 +0900

    Added imagefield in model form, etc

--- moxie/ideas/forms/postidea.py
+++ moxie/ideas/forms/postidea.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 
 from django_summernote.widgets import SummernoteInplaceWidget
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, Fieldset, ButtonHolder
 from crispy_forms.bootstrap import FormActions
 
 from ideas.models import Idea
@@ -14,11 +14,29 @@ class PostIdeaForm(ModelForm):
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Create!'))
+        self.helper.layout = Layout(
+            Div(
+                'title',
+                'thumbnail_image',
+            ),
+            Fieldset(
+                '두번째 단계',
+                'price',
+                'sales_goal',
+                'end_date',
+            ),
+            Fieldset(
+                '마지막 단계',
+                'description',
+                'custom_slug',
+            ),
+        )
 
     class Meta:
         model = Idea
         fields = [
             'title',
+            'thumbnail_image',
             'description',
             'price',
             'sales_goal',
