b72f5171c01ad654f0147a33f59abff42e1ccf68
yongdoree <yongdoree@gmail.com>
2016-04-10 08:51:29 +0900

    modify: form_valid method so that user object is input

--- moxie/ideas/views/idea_create_view.py
+++ moxie/ideas/views/idea_create_view.py
@@ -9,3 +9,7 @@ class IdeaCreateView(CreateView):
     model = Idea
     template_name = "idea_create_view.html"
     form_class = PostIdeaForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(IdeaCreateView, self).form_valid(form)
