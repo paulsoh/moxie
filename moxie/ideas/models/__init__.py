98e871db781773eeca93a4114a8631d7c50720b2
yongdoree <yongdoree@gmail.com>
2016-04-13 15:11:50 +0900

    init comment model

--- moxie/ideas/models/__init__.py
+++ moxie/ideas/models/__init__.py
@@ -1,3 +1,4 @@
 from .idea import Idea
 from .fund import Fund
 from .category import Category
+from .comment import Comment
--- moxie/ideas/views/__init__.py
+++ moxie/ideas/views/__init__.py
@@ -2,3 +2,4 @@ from .idea_create_view import IdeaCreateView
 from .idea_update_view import IdeaUpdateView
 from .idea_detail_view import IdeaDetailView
 from .idea_list_view import IdeaListView
+from .category_list_view import CategoryListView
